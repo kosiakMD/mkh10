c4db7a8f3f0d5297bb28b1e719bb6d4dfbabe369
Anton Kosiak <Keleborn.mail@gmail.com>
2016-04-21 22:58:28 +0300

    Added files via upload

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,9 @@ module.exports = function(grunt) {
 		fonts : 'public/fonts',
 		scripts : 'public/js',
 		src : 'src',
-		bower_path : 'bower_components'
+		srcjs : 'src/js/',
+		bower_path : 'bower_components',
+		jqui : 'bower_components/jquery-ui/ui/minified'
 	};
  
 	grunt.initConfig({
@@ -39,12 +41,13 @@ module.exports = function(grunt) {
 			// Concat CSS files
 			css: {
 				src: [
-					// Bootstrap CSS
+				// Bootstrap CSS
 					// '<%= globalConfig.bower_path %>/bootstrap/dist/css/bootstrap.min.css',
+				// Bootstrap CSS Style
 					'<%= globalConfig.styles %>/spacelab.min.css',
-					// Jasny CSS
+				// Jasny CSS
 					'<%= globalConfig.bower_path %>/jasny-bootstrap/dist/css/jasny-bootstrap.min.css',
-					//My compressed CSS
+				// My compressed CSS
 					'<%= cssmin.dist.dest %>'
 				],
 				dest: '<%= globalConfig.styles %>/all.min.css'
@@ -54,18 +57,34 @@ module.exports = function(grunt) {
 				src: [
 				// Concat JS Libraries
 					// Jquery
-					'<%= globalConfig.bower_path %>/jquery/dist/jquery.min.js',
-					// Jquery UI
-					'<%= globalConfig.bower_path %>/jquery-ui/dist/jquery-ui.min.js',
+						'<%= globalConfig.bower_path %>/jquery/dist/jquery.min.js',
+					// Jquery UI Autocomplete Concat
+						'<%= globalConfig.jqui %>/core.min.js',
+						'<%= globalConfig.jqui %>/widget.min.js',
+						'<%= globalConfig.jqui %>/position.min.js',
+						'<%= globalConfig.jqui %>/menu.min.js',
+						'<%= globalConfig.jqui %>/autocomplete.min.js',
+						// '<%= globalConfig.bower_path %>/jquery-ui/jquery-ui.min.js',
 					// Bootstrap
-					'<%= globalConfig.bower_path %>/bootstrap/dist/js/bootstrap.min.js',
-					// Jasny 
-					'<%= globalConfig.bower_path %>/jasny-bootstrap/dist/jasny-bootstrap.min.js',
+						'<%= globalConfig.bower_path %>/bootstrap/dist/js/bootstrap.min.js',
+					// Jasny-Bootstrap
+						'<%= globalConfig.bower_path %>/jasny-bootstrap/dist/js/jasny-bootstrap.min.js',
 
 				// Concat My JS files
+					// Commons
+						'<%= globalConfig.srcjs %>commons.js',
+					// $http Promise Deffer
+						'<%= globalConfig.srcjs %>$http.js',
+					// Debugger
+						'<%= globalConfig.srcjs %>debugger.js',
+					// App Cache Update
+						'<%= globalConfig.srcjs %>appCacheUpdate.js',
+					// Preloader XHR
+						'<%= globalConfig.srcjs %>preloader.js',
+					// Counter for Menu
+						// '<%= globalConfig.srcjs %>counter.js'
 					// ICD
-					// '<%= globalConfig.scripts %>/icd.js'
-					'<%= uglify.customize.dest %>'
+						'<%= globalConfig.srcjs %>icd.js'
 				],
 				dest: '<%= globalConfig.scripts %>/app.min.js'
 			}
@@ -80,18 +99,23 @@ module.exports = function(grunt) {
 				dest: '<%= globalConfig.scripts %>/<%= pkg.name %>.min.js'
 			},*/
 			customize: {
-				src: '<%= globalConfig.scripts %>/icd.js',
-				dest: '<%= globalConfig.scripts %>/icd.min.js'
+				src: '<%= concat.js.dest %>',
+				dest: '<%= globalConfig.scripts %>/app.min.js'
 			}
 		}
 	});
 
 	// load plugn for task(s)
-	grunt.loadNpmTasks('grunt-yui-compressor');
+	// grunt.loadNpmTasks('grunt-yui-compressor');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// Default task(s).
-	grunt.registerTask('default', [ 'cssmin',/* 'min',*/ 'uglify', 'concat']);
+	grunt.registerTask('default', [ 
+		'cssmin', 
+		// 'min', 
+		'concat',
+		'uglify'
+		]);
 };
